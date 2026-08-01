11da5768021aed0ddeca0aa8a0b0abec7a933835
wryjonue <wryjonue@gmail.com>
2026-08-01 12:45:59 +0800

    Do more setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,48 @@ import { createBrowserRouter, Link, NavLink, Outlet } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
 function Home() {
-  return <h1>Home Page</h1>;
+	return <h1>Home Page</h1>;
 }
 
 function About() {
-  return <h2>About</h2>;
+	return <h2>About</h2>;
 }
 
 function RootLayout() {
-  const [] = useState('');
-  return (
-    <>
-    <nav>
-      <NavLink className="navlink" to="/">Home</NavLink>
-      <NavLink className="navlink" to="/about">About</NavLink>
-    </nav>
-    <Outlet />
-    </>
-  );
+	const [name, setName] = useState('Click to know');
+	return (
+		<>
+			<nav>
+				<NavLink className="navlink" to="/">Home</NavLink>
+				<NavLink className="navlink" to="/about">About</NavLink>
+			</nav>
+			<button
+				onClick={async () => {
+					const res = await fetch("/api/");
+					const body = await res.json();
+					setName(body.name);
+				}}
+			>
+				{name}
+			</button>
+			<Outlet />
+		</>
+	);
 }
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'about', element: <About /> },
-    ],
-  },
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'about', element: <About /> },
+		],
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App
